6d95eb9995f2dce3450a752bc2d5b20cc714891b
Shubham Karmilkar <karmilkarshubham@gmail.com>
2026-02-16 10:01:50 +0530

    CURD fun category

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -34,4 +34,6 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     # Logout
     path('logout/', views.logout, name='logout'),
+    # Dashboard
+    path('dashboard/', include('dashboard.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
