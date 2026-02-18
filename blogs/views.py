a64facd45f0fcf34902f880662736ca18f7fe677
Shubham Karmilkar <karmilkarshubham@gmail.com>
2026-02-18 21:13:06 +0530

    comment feature add

--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,get_object_or_404,redirect
-from blogs.models import Blog,Category
-from django.http import HttpResponse
+from blogs.models import Blog,Category, Comment
+from django.http import HttpResponse, HttpResponseRedirect
 from django.db.models import Q
 
 # Create your views here.
@@ -25,8 +25,22 @@ def posts_by_category(request, category_id):
 
 def blogs(request,slug):
     single_blog = get_object_or_404(Blog, slug = slug, status = 'Published')
+    if request.method == 'POST':
+        comment = Comment()
+        comment.user = request.user
+        comment.blog = single_blog
+        comment.comment = request.POST['comment']
+        comment.save()
+        return HttpResponseRedirect(request.path_info)
+    
+    # Comment
+    comments = Comment.objects.filter(blog = single_blog)
+    comment_count = comments.count()
+    
     context = {
         'single_blog':single_blog,
+        'comments':comments,
+        'comment_count':comment_count,
     }
     return render(request,'blogs.html',context)
 
